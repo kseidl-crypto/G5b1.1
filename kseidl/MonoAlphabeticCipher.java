d36ec8fa7284875434bdd08be26b008bd2c81425
kseidl <kseidl@student.tgm.ac.at>
2020-11-14 18:30:30 +0100

    MonoAlphabeticCipher fertiggestellt

--- kseidl/MonoAlphabeticCipher.java
+++ kseidl/MonoAlphabeticCipher.java
@@ -9,23 +9,33 @@ public class MonoAlphabeticCipher implements Cipher{
     private String secretAlphabet;
 
     public MonoAlphabeticCipher(){
-
+        this.secretAlphabet = "abcdefghijklmnopqrstuvwxyzäöüß";
     }
 
     public String getSecretAlphabet(){
-
+        return this.secretAlphabet;
     }
 
     protected void setSecretAlphabet(String secretAlphabet){
-
+            this.secretAlphabet = secretAlphabet;
 
     }
 
     public String encrypt(String text){
-
+        String standard = "abcdefghijklmnopqrstuvwxyzäöüß";
+        String ergebnis ="";
+        for(int i = 0; i < text.length(); i++){
+            ergebnis += this.secretAlphabet.charAt(standard.indexOf(text.charAt(i)));
+        }
+        return ergebnis;
     }
 
     public String decrypt(String text){
-
+        String standard = "abcdefghijklmnopqrstuvwxyzäöüß";
+        String ergebnis ="";
+        for(int i = 0; i < text.length(); i++){
+            ergebnis += standard.charAt(this.secretAlphabet.indexOf(text.charAt(i)));
+        }
+        return ergebnis;
     }
 }
