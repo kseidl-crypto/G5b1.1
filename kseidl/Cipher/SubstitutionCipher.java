8e9afbe6699d8af1350644331364334128f86872
kseidl <kseidl@student.tgm.ac.at>
2020-11-15 10:48:43 +0100

    subklasse SubstitutionCipher geschrieben

--- kseidl/Cipher/SubstitutionCipher.java
+++ kseidl/Cipher/SubstitutionCipher.java
@@ -3,9 +3,32 @@ package kseidl.Cipher;
 public class SubstitutionCipher extends  MonoAlphabeticCipher{
 
     public SubstitutionCipher(String secretAlphabet){
-
+        setSecretAlphabet(secretAlphabet);
     }
-    public void setSecretAlphabet(String secretAlphabet){
 
+    public void setSecretAlphabet(String secretAlphabet){
+        String standard = "abcdefghijklmnopqrstuvwxyzäöüß";
+        String buchstaben ="";
+        if(secretAlphabet.length() ==30){
+            for(int i = 0; i < secretAlphabet.length(); i++){
+                if(standard.indexOf(secretAlphabet.charAt(i)) != -1){
+                    if(buchstaben.indexOf(secretAlphabet.charAt(i)) == -1){
+                        buchstaben += secretAlphabet.charAt(i);
+                    }
+                    else{
+                        return;
+                    }
+                }
+                else{
+                    return;
+                }
+
+            }
+
+        }
+        else{
+            return;
+        }
+        super.setSecretAlphabet(secretAlphabet);
     }
 }
